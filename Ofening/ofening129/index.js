9929b1a0ec67c0e56e12a3855bbcd268b278986f
Nuno Lcm <nunolcmarques@gmail.com>
2022-09-15 11:49:46 +0200

    oef129

--- Ofening/ofening129/index.js
+++ Ofening/ofening129/index.js
@@ -1,6 +1,6 @@
-let priemgetal = Number(prompt("Getal een number in:"))
+let eindGetal = Number(prompt("Getal een number in:"))
 
-let i = 1;
+/*let i = 1;
 let divisor = 0;
 for (i; i <= priemgetal;i++) {
     if (priemgetal % i === 0) {
@@ -12,4 +12,18 @@ for (i; i <= priemgetal;i++) {
     }else{
         console.log("is not primo ")
     }
+*/
+
+for (let i=1;i<eindGetal;i++){
+    var count = 0;
+    for (let x=1;x<=i;x++){
+        if (i%x === 0){
+            count++
+        }
+    }
+    if (count == 2) {
+        console.log(i);
+    }
+}
+
 
