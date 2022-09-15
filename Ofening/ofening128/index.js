ae09234ee9e6a5767d84075621d3b89dc6a4f34d
Nuno Lcm <nunolcmarques@gmail.com>
2022-09-15 11:06:32 +0200

    oef128

--- Ofening/ofening128/index.js
+++ Ofening/ofening128/index.js
@@ -1,6 +1,6 @@
 let number = Number(prompt("Getal een number in:"))
 
-let i =0;
+/*let i =0;
 let opvangbank = "";
 let y =1;
 for (i;i<=number;i++){
@@ -10,4 +10,17 @@ for (i;i<=number;i++){
     }
 }
     console.log(opvangbank);
-
+Mijn versie
+*/
+gir
+let i = 1;
+let resultaat = " ";
+let x = 1;
+for(i;i<=number;i++){
+    if(x<=number){
+        //console.log(x);
+        resultaat=resultaat + x + ",";
+    }
+    x=x*2;
+}
+console.log(resultaat.substring(0,resultaat.length-1));
